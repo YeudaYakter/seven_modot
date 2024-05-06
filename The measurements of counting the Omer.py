bfd7b2cdc8680e73b6941b4587c53b495009d858
yakter yeuda <143093738+YeudaYakter@users.noreply.github.com>
2024-05-07 01:00:02 +0300

    Update The measurements of counting the Omer.py

--- The measurements of counting the Omer.py
+++ The measurements of counting the Omer.py
@@ -1,4 +1,4 @@
-seven_medot = ["hesed", "gvura", "tefert", "nezah", "hod", "yesod", "malhoot"]
+seven_medot = ["hesed", "gvura", "tefert", "nezah", "hod", "yesod", "malhoot"]"""מערך של 7 הספירות"""
 for i in range(49):
     day = seven_medot[i % 7]
     week = seven_medot[i // 7]
